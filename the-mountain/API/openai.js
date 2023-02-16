6bf6a2ed2ce6cb6a24266ced6f07d428ce879cbe
Orlando-c <lando.j.carcamo@gmail.com>
2023-02-16 08:58:49 -0800

    ftyavgbh

--- the-mountain/API/openai.js
+++ the-mountain/API/openai.js
@@ -1,16 +1,75 @@
-$.ajax({
-    type: 'POST',
-    url: 'https://api.openai.com/v1/engines/text-davinci-002/jobs',
+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
+
+// Import required libraries
+const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
+const fetch = require('node-fetch');
+
+// Set up Express server and SQLite database
+const app = express();
+const port = 3000;
+const db = new sqlite3.Database('responses.db');
+db.run('CREATE TABLE IF NOT EXISTS responses (id INTEGER PRIMARY KEY AUTOINCREMENT, prompt TEXT, response TEXT)');
+
+// Array of possible prompts
+const prompts = [
+  'Make a legend!',
+  'Make a fable!',
+  'Make a short story about an adventure to the peak of a mountain!',
+  'Make a short story about an adventure through a forest!',
+  'Make a story about a villain!',
+  'Make a story about a hero!'
+];
+
+// Set up API endpoint to receive a request and generate a response
+app.get('/response', (req, res) => {
+  // Generate a random index number
+  const randomIndex = Math.floor(Math.random() * prompts.length);
+
+  // Use the OpenAI API to send the prompt to the chatbot and receive a response
+  const apiKey = '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'; // Replace with your actual API key
+  const chatEndpoint = 'https://api.openai.com/v1/engine/text-davinci-003/completions';
+
+  const prompt = prompts[randomIndex];
+  const data = {
+    'prompt': prompt,
+    'max_tokens': 50,
+    'temperature': 0.5,
+    'n': 1,
+    'stop': '\n'
+  };
+
+  fetch(chatEndpoint, {
+    method: 'POST',
     headers: {
-      'Authorization': 'Bearer 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',
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${apiKey}`
     },
-    data: JSON.stringify({
-      'prompt': 'What is your favorite movie?',
-      'max_tokens': 100,
-      'temperature': 0.5
-    }),
-    success: function(data) {
-      $('#response').text(data.choices[0].text);
-    }
+    body: JSON.stringify(data)
+  })
+  .then(response => response.json())
+  .then(responseData => {
+    const responseText = responseData.choices[0].text;
+
+    // Insert the prompt and response into the database
+    db.run('INSERT INTO responses (prompt, response) VALUES (?, ?)', [prompt, responseText], function(error) {
+      if (error) {
+        console.error(error);
+      } else {
+        console.log(`Saved response for prompt: ${prompt}`);
+      }
+    });
+
+    // Send the response text to the frontend as HTML
+    const html = `<h1>OpenAI Chatbot Response</h1><p><strong>Prompt:</strong> ${prompt}</p><p><strong>Response:</strong> ${responseText}</p>`;
+    res.send(html);
+  })
+  .catch(error => {
+    console.error(error);
   });
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
