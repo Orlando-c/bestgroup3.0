589413e5dcc5ff0eb1d6f727d27e158e49bdc05c
Orlando-c <lando.j.carcamo@gmail.com>
2023-02-16 14:25:31 -0800

    baifnf

--- model/users.py
+++ model/users.py
@@ -69,6 +69,7 @@ class Post(db.Model):
 # -- a.) db.Model is like an inner layer of the onion in ORM
 # -- b.) User represents data we want to store, something that is built on db.Model
 # -- c.) SQLAlchemy ORM is layer on top of SQLAlchemy Core, then SQLAlchemy engine, SQL
+
 class User(db.Model):
     __tablename__ = 'users'  # table name is plural, class name is singular
 
@@ -226,3 +227,89 @@ def initUsers():
                 db.session.remove()
                 print(f"Records exist, duplicate email, or error: {user.uid}")
             
+class Responses(db.Model):
+    __tablename__ = 'responses'  # table name is plural, class name is singular
+
+    id = db.Column(db.Integer, primary_key=True)
+    _username = db.Column(db.String(255), unique=True, nullable=False)
+    _aibody = db.Column(db.String(255), unique=False, nullable=False)
+    _userbody = db.Column(db.String(255), unique=False, nullable=False)
+
+    def __init__(self, title, text, imageURL):
+        self._username = title
+        self._aibody = text
+        self._userbody = imageURL
+
+    @property
+    def username(self):
+        return self._username
+  
+    @username.setter
+    def username(self, title):
+        self._username = title
+  
+    @property
+    def aibody(self):
+        return self._aibody
+  
+    @aibody.setter
+    def aibody(self, text):
+        self._aibody = text
+  
+    @property
+    def userbody(self):
+        return self._userbody
+  
+    @userbody.setter
+    def userbody(self, imageURL):
+        self._userbody = imageURL
+
+    def __str__(self):
+        return json.dumps(self.read())
+
+    def create(self):
+        try:
+            db.session.add(self)
+            db.session.commit()
+            return self
+        except IntegrityError:
+            db.session.remove()
+            return None
+
+    def read(self):
+        return {
+            "id": self.id,
+            "Username": self._username,
+            "AI Body": self._aibody,
+            "User Body": self._userbody
+        }
+  
+    def update(self, title="", text="", imageURL=""):
+        if len(title) > 0:
+            self._username = title
+        if len(text) > 0:
+            self._aibody = text
+        if len(imageURL) > 0:
+            self._userbody = imageURL
+        db.session.commit()
+        return self
+
+    def delete(self):
+        db.session.commit()
+        db.session.delete(self)
+        return None
+
+
+def initUsers():
+    with app.app_context():
+        db.init_app(app)
+        db.create_all()
+        u1 = Responses(title="New York City", text="Fortnite", imageURL="amomh.com")
+        u2 = Responses(title="S", text="Among", imageURL="fort.com")
+        users = [u1, u2]
+        for user in users:
+            try:
+                user.create()
+            except IntegrityError:
+                db.session.remove()
+                print(f"Duplicate or error: {user._username}")
