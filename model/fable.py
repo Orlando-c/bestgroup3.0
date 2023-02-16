ed59174f17cb8840b0eb5b17b239c55a312846c5
Orlando-c <lando.j.carcamo@gmail.com>
2023-02-16 15:47:44 -0800

    WHA  THE HEHDHEHEHEEEL LLLL OMG NO WAYAYAYSYA

--- model/fable.py
+++ model/fable.py
@@ -1,92 +1,117 @@
 """ database dependencies to support sqliteDB examples """
-
+from random import randrange
+from datetime import date
+import os, base64
 import json
+
 from __init__ import app, db
 from sqlalchemy.exc import IntegrityError
+from werkzeug.security import generate_password_hash, check_password_hash
 
-class Responses(db.Model):
-    __tablename__ = 'responses'  # table name is plural, class name is singular
 
+''' Tutorial: https://www.sqlalchemy.org/library.html#tutorials, try to get into Python shell and follow along '''
+
+# Define the User class to manage actions in the 'users' table
+# -- Object Relational Mapping (ORM) is the key concept of SQLAlchemy
+# -- a.) db.Model is like an inner layer of the onion in ORM
+# -- b.) User represents data we want to store, something that is built on db.Model
+# -- c.) SQLAlchemy ORM is layer on top of SQLAlchemy Core, then SQLAlchemy engine, SQL
+class Player(db.Model):
+    __tablename__ = 'fables'  # table name is plural, class name is singular
+
+    # Define the User schema with "vars" from object
     id = db.Column(db.Integer, primary_key=True)
     _username = db.Column(db.String(255), unique=True, nullable=False)
-    _aibody = db.Column(db.String(255), unique=False, nullable=False)
-    _userbody = db.Column(db.String(255), unique=False, nullable=False)
+    _userstory = db.Column(db.String(255), unique=False, nullable=False)
+    _aistory = db.Column(db.String(255), unique=False, nullable=False)
 
-    def __init__(self, title, text, imageURL):
-        self._username = title
-        self._aibody = text
-        self._userbody = imageURL
+    # constructor of a User object, initializes the instance variables within object (self)
+    def __init__(self, name, userstory, aistory):
+        self._username = name    # variables with self prefix become part of the object, 
+        self._userstory = userstory
+        self._aistory = aistory
 
+    # a name getter method, extracts name from object
     @property
     def username(self):
         return self._username
-  
-    @username.setter
-    def username(self, title):
-        self._username = title
-  
+    
+    # a setter function, allows name to be updated after initial object creation
+    @name.setter
+    def name(self, name):
+        self._username = name
+    
+    # a getter method, extracts email from object
     @property
-    def aibody(self):
-        return self._aibody
-  
-    @aibody.setter
-    def aibody(self, text):
-        self._aibody = text
-  
-    @property
-    def userbody(self):
-        return self._userbody
-  
-    @userbody.setter
-    def userbody(self, imageURL):
-        self._userbody = imageURL
+    def userstory(self):
+        return self._userstory
+    
+    # a setter function, allows name to be updated after initial object creation
+    @userstory.setter
+    def userstory(self, score):
+        self._userstory = score
 
+    # a getter method, extracts email from object
+    @property
+    def aistory(self):
+        return self._aistory
+    
+    # a setter function, allows name to be updated after initial object creation
+    @aistory.setter
+    def aistory(self, score):
+        self._aistory = score
+    
+    # output content using str(object) in human readable form, uses getter
+    # output content using json dumps, this is ready for API response
     def __str__(self):
         return json.dumps(self.read())
 
+    # CRUD create/add a new record to the table
+    # returns self or None on error
     def create(self):
         try:
-            db.session.add(self)
-            db.session.commit()
+            # creates a person object from User(db.Model) class, passes initializers
+            db.session.add(self)  # add prepares to persist person object to Users table
+            db.session.commit()  # SqlAlchemy "unit of work pattern" requires a manual commit
             return self
         except IntegrityError:
             db.session.remove()
             return None
 
+    # CRUD read converts self to dictionary
+    # returns dictionary
     def read(self):
         return {
             "id": self.id,
-            "Username": self._username,
-            "AI Body": self._aibody,
-            "User Body": self._userbody
+            "username": self.name,
+            "user-story": self.userstory,
+            "AI-story": self.aistory,
         }
-  
-    def update(self, title="", text="", imageURL=""):
-        if len(title) > 0:
-            self._username = title
-        if len(text) > 0:
-            self._aibody = text
-        if len(imageURL) > 0:
-            self._userbody = imageURL
+
+    # CRUD update: updates user name, password, phone
+    # returns self
+    def update(self, name="", score=""):
+        """only updates values with length"""
+        if len(name) > 0:
+            self.name = name
+        if len(score) > 0:
+            self.userstory = score
         db.session.commit()
         return self
 
+    # CRUD delete: remove self
+    # None
     def delete(self):
-        db.session.commit()
         db.session.delete(self)
+        db.session.commit()
         return None
 
 
-def initUsers():
-    with app.app_context():
-        db.init_app(app)
-        db.create_all()
-        u1 = Responses(title="New York City", text="Fortnite", imageURL="amomh.com")
-        u2 = Responses(title="S", text="Among", imageURL="fort.com")
-        users = [u1, u2]
-        for user in users:
-            try:
-                user.create()
-            except IntegrityError:
-                db.session.remove()
-                print(f"Duplicate or error: {user._username}")
+"""Database Creation and Testing """
+
+
+# Builds working data for testing
+def initFables():
+    """Create database and tables"""
+    db.create_all()
+    """Tester data for table"""
