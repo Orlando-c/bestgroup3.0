82eb6fb1301eff7043c55a628ddfb6248f669018
raisinbran25 <jadenn372@gmail.com>
2023-02-16 10:42:47 -0800

    cds

--- api/scores.py
+++ api/scores.py
@@ -2,15 +2,15 @@ from flask import Blueprint, request, jsonify
 from flask_restful import Api, Resource # used for REST API building
 from datetime import datetime
 
-from model.players import Player
+from model.users import User
 
-player_api = Blueprint('player_api', __name__,
-                   url_prefix='/api/scores')
+user_api = Blueprint('user_api', __name__,
+                   url_prefix='/api/users')
 
 # API docs https://flask-restful.readthedocs.io/en/latest/api.html
-api = Api(score_api)
+api = Api(user_api)
 
-class PlayerAPI:        
+class UserAPI:        
     class _Create(Resource):
         def post(self):
             ''' Read data for json body '''
@@ -22,29 +22,41 @@ class PlayerAPI:
             if name is None or len(name) < 2:
                 return {'message': f'Name is missing, or is less than 2 characters'}, 210
             # validate uid
-            score = body.get('score')
-            if score is None or len(score) < 2:
+            uid = body.get('uid')
+            if uid is None or len(uid) < 2:
                 return {'message': f'User ID is missing, or is less than 2 characters'}, 210
+            # look for password and dob
+            password = body.get('password')
+            dob = body.get('dob')
 
             ''' #1: Key code block, setup USER OBJECT '''
-            uo = Player(name=name, 
-                      score=score)
+            uo = User(name=name, 
+                      uid=uid)
             
             ''' Additional garbage error checking '''
             # set password if provided
+            if password is not None:
+                uo.set_password(password)
+            # convert to date type
+            if dob is not None:
+                try:
+                    uo.dob = datetime.strptime(dob, '%Y-%m-%d').date()
+                except:
+                    return {'message': f'Date of birth format error {dob}, must be mm-dd-yyyy'}, 210
+            
             ''' #2: Key Code block to add user to database '''
             # create user in database
-            player = uo.create()
+            user = uo.create()
             # success returns json of user
-            if player:
-                return jsonify(player.read())
+            if user:
+                return jsonify(user.read())
             # failure returns error
-            return {'message': f'Processed {name}, either a format error or User ID {score} is duplicate'}, 210
+            return {'message': f'Processed {name}, either a format error or User ID {uid} is duplicate'}, 210
 
     class _Read(Resource):
         def get(self):
-            players = Player.query.all()    # read/extract all users from database
-            json_ready = [player.read() for player in players]  # prepare output in json
+            users = User.query.all()    # read/extract all users from database
+            json_ready = [user.read() for user in users]  # prepare output in json
             return jsonify(json_ready)  # jsonify creates Flask response object, more specific to APIs than json.dumps
 
     # building RESTapi endpoint
